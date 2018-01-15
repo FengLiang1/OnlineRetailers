843701f21d694cf96e366b241f8f7d02e9a688dc
FengLiang1 <nyxfcq@163.com>
2018-01-15 21:38:13 +0800

    添加实体类 Product, User

---
+++ seckill/src/main/java/com/lemon/entity/Product.java
@@ -0,0 +1,12 @@
+package com.lemon.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String id;
+    private String name;
+    private String startTime;
+    private String endTime;
+    private String createTime;
+}
